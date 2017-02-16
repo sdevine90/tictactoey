a5215bcd1928a03534b20251319ca4b21f76b253
Stephanie <s.devine90@gmail.com>
2017-02-16 15:15:14 +0000

    epic stylings

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Tile = (props) => {
+  if (props.text === 'x'){
+    var src= "../image/boots.png"
+    }else if (props.text === 'o'){
+      var src= "../image/beard.png"
+    } else {
+
+    }
+  
   console.log(props);
   return (
-    <div className="tile" onClick={props.func}>{props.text}</div>
+    <div className="tile" onClick={props.func}>   
+    <img src= {src} ></img>
+    </div>
     )
 }
 
--- client/src/containers/TicContainer.jsx
+++ client/src/containers/TicContainer.jsx
@@ -12,84 +12,106 @@ class TicContainer extends React.Component {
   winMethod (){
     this.rowWinMethod()
     this.columnWinMethod()
-    this.diagnolWinMethod()
+    this.diagonalWinMethod()
   }
 
-  columnWinMethod(){
+  diagonalWinMethod(){
     var rowOne = this.state.gameArrays[0]
     var rowTwo = this.state.gameArrays[1]
     var rowThree = this.state.gameArrays[2]
-    for (var i = 0; i < this.state.gameArrays[0].length; i++){
-     if ((rowOne[i] === rowTwo[i]) && (rowOne[i] === rowThree[i])){
-      if (rowOne[i] === 'x'){
-          console.log('x wins') 
-        } else if (rowOne[i] === 'o'){
-          console.log('o wins')
-        }
+    if ((rowOne[0] === rowTwo[1]) && (rowOne[0] === rowThree[2])){
+     if (rowOne[0] === 'x'){
+       alert('x wins with diagonal') 
+     } else if (rowOne[0] === 'o'){
+       alert('o wins with diagonal')
      }
+   }
+    if ((rowOne[2] === rowTwo[1]) && (rowOne[2] === rowThree[0])){
+     if (rowOne[2] === 'x'){
+       alert('x wins with diagonal') 
+     } else if (rowOne[2] === 'o'){
+       alert('o wins with diagonal')
+     }
+   }
+ }
+
+ columnWinMethod(){
+  var rowOne = this.state.gameArrays[0]
+  var rowTwo = this.state.gameArrays[1]
+  var rowThree = this.state.gameArrays[2]
+  for (var i = 0; i < this.state.gameArrays[0].length; i++){
+   if ((rowOne[i] === rowTwo[i]) && (rowOne[i] === rowThree[i])){
+    if (rowOne[i] === 'x'){
+      alert('x wins with column') 
+    } else if (rowOne[i] === 'o'){
+      alert('o wins with column')
     }
   }
-  rowWinMethod (){
-    for (var i = 0; i < this.state.gameArrays.length; i++){
-      var row = this.state.gameArrays[i]
-      console.log(row)
-      if ((row[0] === row[1]) && (row[0] === row[2])){
-        if (row[0] === 'x'){
-          console.log('x wins') 
-        } else if (row[0] === 'o'){
-          console.log('o wins')
-        }
+}
+}
+rowWinMethod (){
+  for (var i = 0; i < this.state.gameArrays.length; i++){
+    var row = this.state.gameArrays[i]
+    if ((row[0] === row[1]) && (row[0] === row[2])){
+      if (row[0] === 'x'){
+        alert('x wins with row') 
+      } else if (row[0] === 'o'){
+        alert('o wins with row')
       }
     }
   }
-  clickyClick (array, pos){
+}
+clickyClick (array, pos){
 
-    console.log("you clicked a square, it was at position", pos, "in row", array);
+  console.log("you clicked a square, it was at position", pos, "in row", array);
 
 
-    if (this.gameArrays[array][pos] === null){
-      this.gameArrays[array][pos] = this.turn;
-      console.log(this.gameArrays);
-      if (this.turn === 'x'){
-        this.turn = 'o';
-      } else if (this.turn === 'o') {
-        this.turn = 'x';
-      } 
-    } else {
-      console.log("Sorry, that square's taken!!")
-    }
-    this.setState({gameArrays: this.gameArrays, turn: this.turn})
-    this.winMethod()
+  if (this.gameArrays[array][pos] === null){
+    this.gameArrays[array][pos] = this.turn;
+    console.log(this.gameArrays);
+    if (this.turn === 'x'){
+      this.turn = 'o';
+    } else if (this.turn === 'o') {
+      this.turn = 'x';
+    } 
+  } else {
+    console.log("Sorry, that square's taken!!")
   }
+  this.setState({gameArrays: this.gameArrays, turn: this.turn})
+  this.winMethod()
+}
 
-  render(){
-    return (
-      <div>
-      <h3>It is the turn of player {this.state.turn}</h3>
-      <div className = "Grid">
+render(){
+  return (
+    <div>
+    <div className= "bigContainer">
+    <h3>It is the turn of player {this.state.turn}</h3>
+    </div>
+    <div className= "bigContainer">
+    <div className = "Grid">
 
-      <div className= "rowOne">
-      <Tile func={ function(){this.clickyClick(0, 0)}.bind(this) } text={this.state.gameArrays[0][0]} ></Tile>
-      <Tile func={ function(){this.clickyClick(0, 1)}.bind(this) } text={this.state.gameArrays[0][1]}></Tile>
-      <Tile func={ function(){this.clickyClick(0, 2)}.bind(this) } text={this.state.gameArrays[0][2]}></Tile>
-      </div>
+    <div className= "rowOne">
+    <Tile func={ function(){this.clickyClick(0, 0)}.bind(this) } text={this.state.gameArrays[0][0]} ></Tile>
+    <Tile func={ function(){this.clickyClick(0, 1)}.bind(this) } text={this.state.gameArrays[0][1]}></Tile>
+    <Tile func={ function(){this.clickyClick(0, 2)}.bind(this) } text={this.state.gameArrays[0][2]}></Tile>
+    </div>
 
-      <div className= "rowTwo">
-      <Tile func={ function(){this.clickyClick(1, 0)}.bind(this) } text={this.state.gameArrays[1][0]} ></Tile>
-      <Tile func={ function(){this.clickyClick(1, 1)}.bind(this) } text={this.state.gameArrays[1][1]} ></Tile>
-      <Tile func={ function(){this.clickyClick(1, 2)}.bind(this) } text={this.state.gameArrays[1][2]} ></Tile>
-      </div>
+    <div className= "rowTwo">
+    <Tile func={ function(){this.clickyClick(1, 0)}.bind(this) } text={this.state.gameArrays[1][0]} ></Tile>
+    <Tile func={ function(){this.clickyClick(1, 1)}.bind(this) } text={this.state.gameArrays[1][1]} ></Tile>
+    <Tile func={ function(){this.clickyClick(1, 2)}.bind(this) } text={this.state.gameArrays[1][2]} ></Tile>
+    </div>
 
-      <div className= "rowThree">
-      <Tile func={ function(){this.clickyClick(2, 0)}.bind(this) } text={this.state.gameArrays[2][0]} ></Tile>
-      <Tile func={ function(){this.clickyClick(2, 1)}.bind(this) } text={this.state.gameArrays[2][1]} ></Tile>
-      <Tile func={ function(){this.clickyClick(2, 2)}.bind(this) } text={this.state.gameArrays[2][2]} ></Tile>
-      </div>
-
-      </div>
-      </div>
-      )
-  }
+    <div className= "rowThree">
+    <Tile func={ function(){this.clickyClick(2, 0)}.bind(this) } text={this.state.gameArrays[2][0]} ></Tile>
+    <Tile func={ function(){this.clickyClick(2, 1)}.bind(this) } text={this.state.gameArrays[2][1]} ></Tile>
+    <Tile func={ function(){this.clickyClick(2, 2)}.bind(this) } text={this.state.gameArrays[2][2]} ></Tile>
+    </div>
+    </div>
+    </div>
+    </div>
+    )
+}
 }
 
 export default TicContainer;
